9f567b2f813137209de3de758dfb1440235c2f13
Anton <anton.solovev.1990@mail.ru>
2023-05-23 14:48:34 +0700

    1

--- webpack.config.js
+++ webpack.config.js
@@ -3,40 +3,45 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-      },
-    }, {
-      test: /\.html$/,
-      use: [{
-        loader: 'html-loader',
-      }],
-    },
-    {
-      test: /\.css$/,
-      use: [
-        MiniCssExtractPlugin.loader, 'css-loader',
-      ],
-    },
-    {
-      test: /\.(png|jpg|gif)$/i,
-      use: [
+    rules: [
         {
-          loader: 'url-loader',
-          options: {
-            limit: 8192,
+            test: /\.(png|jpg|gif)$/i,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 8192,
+                }
+              },
+            ],
+           type: 'javascript/auto'
           },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-      ],
-    },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
     ],
   },
   plugins: [
