2bdfbd36afd87fc6e2c958fb599d19efd72c0cee
Anton <anton.solovev.1990@mail.ru>
2023-05-31 13:06:21 +0700

    1

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
